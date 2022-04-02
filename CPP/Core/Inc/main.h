3b14bd783bb3526c8a33f4c6e82af70c48b04034
Mark Sherstan <mshersta@ualberta.ca>
2022-04-02 16:30:18 -0600

    Configure pins

--- CPP/Core/Inc/main.h
+++ CPP/Core/Inc/main.h
@@ -69,8 +69,8 @@ void Error_Handler(void);
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
 #define TCK_GPIO_Port GPIOA
-#define SWO_Pin GPIO_PIN_3
-#define SWO_GPIO_Port GPIOB
+#define SPI1_CS_Pin GPIO_PIN_6
+#define SPI1_CS_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
