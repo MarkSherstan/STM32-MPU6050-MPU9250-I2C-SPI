40c2dc7b171fdf77d39b42c15a04e35d7fde00f9
Mark Sherstan <mshersta@ualberta.ca>
2022-04-02 19:29:03 -0600

    Initial commit

---
+++ CPP/Core/Inc/MPU9250.h
@@ -0,0 +1,16 @@
+/*
+ * MPU9250.h
+ *
+ *  Created on: Apr 2, 2022
+ *      Author: MarkSherstan
+ */
+
+#ifndef SRC_MPU9250_H_
+#define SRC_MPU9250_H_
+
+class MPU9250 {
+public:
+	MPU9250();
+};
+
+#endif /* SRC_MPU9250_H_ */
---
+++ CPP/Core/Src/MPU9250.cpp
@@ -0,0 +1,14 @@
+/*
+ * MPU9250.cpp
+ *
+ *  Created on: Apr 2, 2022
+ *      Author: MarkSherstan
+ */
+
+#include "MPU9250.h"
+
+MPU9250::MPU9250() {
+	// TODO Auto-generated constructor stub
+
+}
+
